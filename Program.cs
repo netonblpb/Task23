7b00957060ec52ac1daba981689bd7f752f1db98
netonblpb <skullbog@gmail.com>
2022-02-11 13:08:08 +0300

    Task23 complete

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int i = 1;
+int step = 1;
+while (i < num +1)
+{
+    //step = i*i*i;
+    Console.Write($"{i} в третьей степени равно {Math.Pow(i, 3)}");
+    Console.WriteLine();
+    i++;
+}
