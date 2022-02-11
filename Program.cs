9570426ab62d4b272b5af28672cb1327cac5ebd6
netonblpb <skullbog@gmail.com>
2022-02-11 13:14:16 +0300

    Pow correction

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ int step = 1;
 while (i < num +1)
 {
     //step = i*i*i;
-    Console.Write($"{i} в третьей степени равно {Math.Pow(i, 3)}");
+    Console.Write($"{i} в квадрате степени равно {Math.Pow(i, 2)}");
     Console.WriteLine();
     i++;
 }
